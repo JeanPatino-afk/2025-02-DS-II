8f4b2f4a851e296b8d86da5cc177425ad7bd8748
JeanPatino-afk <patinomoreloj@gmail.com>
2025-11-18 23:00:05 -0500

    Delete 2025-02-DS-II---Spring-Security-proyectofinal directory

--- 2025-02-DS-II---Spring-Security-proyectofinal/src/main/java/spring_security/plataforma_online/repository/OrdenRepository.java
+++
@@ -1,7 +0,0 @@
-package spring_security.plataforma_online.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import spring_security.plataforma_online.model.OrdenDeCompra;
-
-public interface OrdenRepository extends JpaRepository<OrdenDeCompra, Long> {
-}
